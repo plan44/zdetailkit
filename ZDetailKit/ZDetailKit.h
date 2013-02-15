edfe127a1a582833c33929120ef5650812465bdc
Lukas Zeller <luz@plan44.ch>
2013-02-15 18:06:44 +0100

    Added ZDetailKit.h umbrella header file

---
+++ ZDetailKit/ZDetailKit.h
@@ -0,0 +1,27 @@
+//
+//  ZDetailKit.h
+//
+//  Created by Lukas Zeller on 15.02.13.
+//  Copyright (c) 2013 plan44.ch. All rights reserved.
+//
+
+
+// common
+#import "ZDetailEditing.h"
+#import "ZTextExpanderSupport.h"
+
+// controllers
+#import "ZDetailTableViewController.h"
+
+// cells
+#import "ZButtonCell.h"
+#import "ZSwitchCell.h"
+#import "ZSegmentChoicesCell.h"
+#import "ZSliderCell.h"
+#import "ZTextFieldCell.h"
+#import "ZTextViewCell.h"
+#import "ZDateTimeCell.h"
+#import "ZColorChooserCell.h"
+#import "ZChoiceListCell.h"
+
+// EOF
