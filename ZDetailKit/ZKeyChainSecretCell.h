6266d9f147c27446bc9bdc676f565ea2c96c75ac
Lukas Zeller <luz@plan44.ch>
2013-02-15 18:10:09 +0100

    ZkeyChainSecretCell: cell to edit a password stored in the keychain (using ZKeyChainWrapper)

---
+++ ZDetailKit/ZKeyChainSecretCell.h
@@ -0,0 +1,16 @@
+//
+//  ZKeyChainSecretCell.h
+//
+//  Created by Lukas Zeller on 2011/08/16.
+//  Copyright 2013 plan44.ch. All rights reserved.
+//
+
+#import "ZTextFieldCell.h"
+
+
+@interface ZKeyChainSecretCell : ZTextFieldCell
+
+@property (retain, nonatomic) NSString *keyChainService;
+@property (retain, nonatomic) NSString *keyChainAccount;
+
+@end
---
+++ ZDetailKit/ZKeyChainSecretCell.m
@@ -0,0 +1,53 @@
+//
+//  ZKeyChainSecretCell.m
+//
+//  Created by Lukas Zeller on 2011/08/16.
+//  Copyright 2013 plan44.ch. All rights reserved.
+//
+
+#import "ZKeyChainSecretCell.h"
+
+#import "ZKeyChainWrapper.h"
+
+
+@interface ZKeyChainSecretCell ()
+
+@property (retain, nonatomic) NSString *secret;
+
+@end
+
+
+@implementation ZKeyChainSecretCell
+
+
+- (id)initWithStyle:(ZDetailViewCellStyle)aStyle reuseIdentifier:(NSString *)aReuseIdentifier
+{
+  if ((self = [super initWithStyle:aStyle reuseIdentifier:aReuseIdentifier])) {
+    // auto-configure as password cell
+    self.secureTextEntry = YES;
+    // auto-connect with internal property which accesses the keychain
+    [self.valueConnector connectTo:self keyPath:@"secret"];
+  }
+  return self;
+}
+
+
+
+- (void)setSecret:(NSString *)secret
+{
+  if (_keyChainService && _keyChainAccount) {
+    [[ZKeyChainWrapper sharedKeyChainWrapper] setPassword:secret forService:_keyChainService account:_keyChainAccount error:NULL];
+  }
+}
+
+
+- (NSString *)secret
+{
+  if (_keyChainService && _keyChainAccount) {
+    return [[ZKeyChainWrapper sharedKeyChainWrapper] passwordForService:key account:object error:NULL];
+  }
+  return nil;
+}
+
+
+@end // ZKeyChainSecretCell
